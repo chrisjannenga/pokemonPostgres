4b2548a84aa7744b9c8c3cef575d91811b5400ba
chrisjannenga <Cjannenga312@gmail.com>
2017-08-15 10:07:13 -0700

    committed stuffs

--- pokemon/routes/pokemon.js
+++ pokemon/routes/pokemon.js
@@ -17,13 +17,38 @@ router.get('/', function(req, res, next) {
         title: 'Express',
         allPokemon: data.rows
       });
-    })
-    .catch(function(err) {
-      /*throw err;*/
-      console.log(err);
-      res.json(err);
-    })
+    });
+  .catch(function(err) {
+    /*throw err;*/
+    console.log(err);
+    res.json(err);
+  });
   console.log("HELLOOOOO")
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('newForm', {
+    title: "New"
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  let id = req.params.id;
+  knex.raw(`SELECT * FROM pokemons WHERE id = ${req.params.id}`)
+    .then(function(data) {
+      console.log(data.rows)
+      res.render('allPokemon', {
+        title: 'Express',
+        allPokemon: data.rows
+      });
+    });
+  .catch(function(err) {
+    /*throw err;*/
+    console.log(err);
+    res.json(err);
+  });
+});
+
+
 
 module.exports = router;
--- pokemon/routes/trainers.js
+++ pokemon/routes/trainers.js
@@ -8,11 +8,40 @@ var knex = require('knex')({
   }
 });
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Trainers'
-  });
+  knex.raw('SELECT name FROM trainers;')
+    .then(function(data) {
+      console.log(data.rows)
+      res.render('allPokemon', {
+        title: 'Express',
+        allPokemon: data.rows
+      });
+    })
+    .catch(function(err) {
+      /*throw err;*/
+      console.log(err);
+      res.json(err);
+    })
+  console.log("HELLOOOOO")
 });
+
+router.get('/:id', function(req, res, next) {
+  let id = req.params.id;
+  knex.raw(`SELECT * FROM trainers WHERE id = ${req.params.id}`)
+    .then(function(data) {
+      console.log(data.rows)
+      res.render('allPokemon', {
+        title: 'Express',
+        allPokemon: data.rows
+      });
+    })
+    .catch(function(err) {
+      /*throw err;*/
+      console.log(err);
+      res.json(err);
+    })
+})
 
 module.exports = router;
